ab447bef343607632a1308e4d679b3b3c2a50632
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-24 20:44:47 -0300

    lint-staged ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TaskList } from './components/TaskList'
 import { Header } from './components/Header'
 import './styles/global.scss'
 
-const -t = ''
+const --t = ''
 
 export function App() {
   return (
