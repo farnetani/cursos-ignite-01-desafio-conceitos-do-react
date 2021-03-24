21be2de43a2d2ca62a772f37b0b4a984e78173e1
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-03-24 20:20:47 -0300

    Teste 01

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { TaskList } from './components/TaskList'
 import { Header } from './components/Header'
 import './styles/global.scss'
 
+const t = ''
+
 export function App() {
   return (
     <>
